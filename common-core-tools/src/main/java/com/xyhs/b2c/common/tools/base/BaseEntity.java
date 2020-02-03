d191bb0933299e34167961659b348f829468dfcf
xyhs <as8872770@126.com>
2020-02-03 11:41:04 +0800

    添加反序列初始化

--- common-core-tools/src/main/java/com/xyhs/b2c/common/tools/base/BaseEntity.java
+++ common-core-tools/src/main/java/com/xyhs/b2c/common/tools/base/BaseEntity.java
@@ -1,11 +1,14 @@
 package com.xyhs.b2c.common.tools.base;
 
+import java.io.Serializable;
+
 /**
  * @author ljp
  * @apiNote
  * @date 14:24 2019/12/30
  **/
 
-public class BaseEntity {
+public class BaseEntity   implements Serializable {
 
+    private static final long serialVersionUID = -8239034348377641656L;
 }
